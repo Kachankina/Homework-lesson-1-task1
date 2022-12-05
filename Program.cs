b08cb4367951b78551701d90e59c59d535773305
Katya <www@mail.ru>
2022-12-05 14:20:51 +0300

    two

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 1
+Console.WriteLine("Please, insert first number");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please, insert second number");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{
+    Console.WriteLine("First number:" + A + "more than" + B);
+}
+else
+{
+    Console.WriteLine("Second number:" + B + "more than" + A);
+}
+
